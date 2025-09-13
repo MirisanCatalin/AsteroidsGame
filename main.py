0938f75216c1d4f6ced4ca979fa06d8f9de46edb
Mirisan Catalin <catalinmirisan1@gmail.com>
2025-09-13 19:27:51 +0300

    creating groups

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
 
+
 def main():
-
     pygame.init()
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-
-    player = Player(x, y)
-
-    player.draw(screen)
-
-
     while True:
-
-        screen.fill("black")
-
-        player.draw(screen)
-
-        pygame.display.flip()
-
-        player.update(dt)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-        
-        pygame.display.set_caption(f"Asteroids - FPS: {clock.get_fps():.2f}")
+
 
 if __name__ == "__main__":
     main()
+
