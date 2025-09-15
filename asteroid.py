475925479cf61d9660c8eafe3032105ec37c9735
Mirisan Catalin <catalinmirisan1@gmail.com>
2025-09-15 11:33:55 +0300

    Lumpy asteroids

--- asteroid.py
+++ asteroid.py
@@ -2,13 +2,31 @@ from circleshape import CircleShape
 from constants import *
 import pygame
 import random
+import math
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-    
+        self.shape = self.generate_shape() 
+
+    def generate_shape(self):
+        points = []
+        num_points = 20  # more points = smoother outline
+        for i in range(num_points):
+            angle = (2 * math.pi / num_points) * i
+            noisy_radius = self.radius * (1 + random.uniform(-0.3, 0.3))  # add lumpiness
+            x = noisy_radius * math.cos(angle)
+            y = noisy_radius * math.sin(angle)
+            points.append((x, y))
+        return points
+
+#    def draw(self, screen):
+#        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        # Shift local shape points by asteroid position
+        translated = [(self.position.x + px, self.position.y + py) for px, py in self.shape]
+        pygame.draw.polygon(screen, "white", translated, 5)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
